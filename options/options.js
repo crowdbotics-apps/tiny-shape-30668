ffa471faf00b4138f40cdfe4bd0495cf0b539066
Crowdbotics <team@crowdbotics.com>
2021-09-18 23:32:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tiny_shape_30668Identifier",
+  url: "https://tiny_shape_30668Identifier.botics.co",
+  api: "https://tiny_shape_30668Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
